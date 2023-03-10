108d3bdf5d827333a39ba0801cc294231dc40093
Gustavo Alves <gugaalves92@hotmail.com>
2023-03-09 21:18:01 -0300

    Add project files.

---
+++ src/Puc.BnccTeste.Data/Repositorio/RepositorioBase.cs
@@ -0,0 +1,33 @@
+﻿using Puc.BnccTeste.Infra.Data.Context;
+using Puc.BnccTeste.Infra.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puc.BnccTeste.Infra.Data.Repositorio
+{
+    public class RepositorioBase<TEntity> : IRepositorio<TEntity> where TEntity : class
+    {
+        public RepositorioBase(Contexto contexto)
+        {
+        }
+
+        public IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity ObterPeloId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Puc.BnccTeste.Infra.CrossCutting.DI/InjecaoDependencia/ResolveInjecao.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Puc.BnccTeste.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Puc.BnccTeste.Infra.CrossCutting.DI.InjecaoDependencia
+{
+    public static class ResolveInjecao
+    {
+        public static void RegisterService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<Contexto>(
+                options => options.UseSqlServer(configuration.GetConnectionString("BnccTesteConnection")));
+        }
+    }
+}
